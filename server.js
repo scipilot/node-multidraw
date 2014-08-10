3741f32b544d70f6c883268d4274498092a15672
Tim Sullivan <timaaarrreee@gmail.com>
2014-08-10 18:20:01 +1200

    more monitoring

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ var express = require('express'), app = express();
 var server = http.createServer(app).listen(port);
 var jade = require('jade');
 var io = require('socket.io').listen(server);
+var os = require('os')
 
 var drawActionStack = [];
 var connectedClients = 0;
 var clients = {};
+var netUsage = 0;
 
 app.use(express.logger());
 app.set('views', __dirname + '/views');
@@ -28,6 +30,7 @@ io.sockets.on('connection', function(socket){
 
     connectedClients += 1;
     socket.emit('drawActionHistory', drawActionStack);
+    netUsage += sizeof(drawActionStack);
 
     socket.on('mousemove', function(data){
 	if(data.id in clients){
@@ -55,10 +58,12 @@ io.sockets.on('connection', function(socket){
 	clients[data.id].x = data.x;
 	clients[data.id].y = data.y;
 
+	netUsage += sizeof(data) * connectedClients;
 	socket.broadcast.emit('moving', data);
     });
 
     socket.on('chatmessage', function(data){
+	netUsage += sizeof(data) * connectedClients;
 	data.message = data.message.replace(/(<([^>]+)>)/ig,""); //take out possible tags.
 	//limit chat message length.
 	if (data.message.length <= 135) {
@@ -72,14 +77,39 @@ io.sockets.on('connection', function(socket){
     socket.on('ping', function(id){
 	var data = {
 	    drawStackSize: drawActionStack.length,
-	    connectedClients: connectedClients
+	    connectedClients: connectedClients,
+	    load1: (os.loadavg()[0]).toFixed(2),
+	    netUsageKB: (netUsage / 1000).toFixed(1)
 	};
+	netUsage += sizeof(data);
 	socket.emit('pong', data);
     });
 
     socket.on('disconnect', function() {
 	connectedClients--; 
     });
-
 });
-	      
+
+function sizeof(object) {
+    var objectList = [];
+    var stack = [object];
+    var bytes = 0;
+
+    while (stack.length) {
+        var value = stack.pop();
+
+        if (typeof value === 'boolean' ) {
+            bytes += 4;
+        } else if (typeof value === 'string') {
+            bytes += value.length * 2;
+        } else if (typeof value === 'number') {
+            bytes += 8;
+        } else if (typeof value === 'object' && objectList.indexOf( value ) === -1) {
+            objectList.push(value);
+            for(var i in value) {
+                stack.push(value[i]);
+            }
+        }
+    }
+    return bytes;
+}
